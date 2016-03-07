262b4cf50e5cda5e0ea86048e83b39b4d3e3c1f8
Mitul Mistry <mitul.mistry1@gmail.com>
2016-03-07 06:19:54 +0000

    models

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,16 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
 
+  belongs_to :user
+
+  def total
+    sum = 0
+    self.items.each { |item| sum += item.price } #ALSO WORKS: self.items.collect { |item| item.price }.inject(0, :+)
+    sum
+  end
+
+  def add_item(item_id)
+    
+  end
 end
